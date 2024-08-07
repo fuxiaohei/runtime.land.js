8cdfc1e9136d97d4d9cd14295375f9ce9ebe1010
fuxiaohei <fuxiaohei@vip.qq.com>
2024-08-07 23:05:51 +0800

    init handle_js_requests function, build request args into js vendor to call fetch event

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,10 @@
 use anyhow::{anyhow, Result};
+use http::StatusCode;
 use once_cell::sync::OnceCell;
+use rquickjs::function::Args;
 use rquickjs::loader::{BuiltinLoader, BuiltinResolver};
-use rquickjs::Module;
+use rquickjs::{Module, Object};
+use std::collections::HashMap;
 use std::io::Read;
 
 static PKG_VERSION: &str = env!("CARGO_PKG_VERSION");
@@ -75,15 +78,72 @@ fn init_js_context() -> Result<()> {
     Ok(())
 }
 
-use land_sdk::http::{Body, Error, Request, Response};
+use land_sdk::http::{error_response, Body, Error, Request, Response};
 use land_sdk::http_main;
 
 #[http_main]
 pub fn handle_request(req: Request) -> Result<Response, Error> {
-    make_response()
+    let resp = match handle_js_request(req) {
+        Ok(response) => response,
+        Err(err) => {
+            println!("handle_js_request error: {:?}", err);
+            error_response(StatusCode::INTERNAL_SERVER_ERROR, err.to_string())
+        }
+    };
+    Ok(resp)
 }
 
-fn make_response() -> Result<Response, Error> {
+fn handle_js_request(req: Request) -> Result<Response, Error> {
+    let context = JS_CONTEXT.get().unwrap();
+    // convert http request to js request
+    let mut headers = HashMap::new();
+    req.headers().iter().for_each(|(key, value)| {
+        headers.insert(
+            key.as_str().to_string(),
+            value.to_str().unwrap().to_string(),
+        );
+    });
+
+    let response_result = context.with(|ctx| {
+        // 0. getCallHandler
+        let call_handler: rquickjs::Value = ctx.globals().get("callHandler")?;
+        if !call_handler.is_function() {
+            let err = ctx.throw(
+                rquickjs::String::from_str(ctx.clone(), "fetch handler is not a function")?
+                    .into_value(),
+            );
+            return Err(err);
+        }
+        let call_handler = call_handler.as_function().unwrap();
+
+        // 1. build request object
+        let headers_object = Object::new(ctx.clone())?;
+        req.headers().iter().for_each(|(key, value)| {
+            headers_object
+                .set(
+                    key.as_str().to_string(),
+                    value.to_str().unwrap().to_string(),
+                )
+                .unwrap();
+        });
+        let req_object = Object::new(ctx.clone())?;
+        req_object.set("id", 1)?;
+        req_object.set("method", req.method().to_string())?;
+        req_object.set("uri", req.uri().clone().to_string())?;
+        req_object.set("headers", headers_object)?;
+        req_object.set("body", req.body().body_handle())?;
+        let mut args = Args::new(ctx.clone(), 1);
+        args.push_arg(req_object)?;
+        let call_handler_result: rquickjs::Result<rquickjs::Value> = call_handler.call_arg(args);
+        if call_handler_result.is_err() {
+            return Err(call_handler_result.unwrap_err());
+        }
+        Ok::<_, rquickjs::Error>(rquickjs::Undefined)
+    });
+    if response_result.is_err() {
+        return Err(response_result.unwrap_err().into());
+    }
+
     let mut resp = Response::new(Body::from("Hello World!"));
     resp.headers_mut()
         .insert("X-runtime-land-js", PKG_VERSION.parse().unwrap());
