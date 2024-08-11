255028fb151e210dc01d6e0fdcdcf0f14d5d3fe5
fuxiaohei <fuxiaohei@vip.qq.com>
2024-08-11 14:34:10 +0800

    update workflow install-deps.sh

--- install-deps.sh
+++ install-deps.sh
@@ -8,6 +8,6 @@ tar -xzf wasi-sdk-24.0-linux.tar.gz
 export WASI_SDK=$(pwd)/wasi-sdk-24.0
 
 echo "Installing wizer"
-curl --fail --location --silent https://github.com/bytecodealliance/wizer/releases/download/v6.0.0/wizer-v6.0.0-x86_64-linux.tar.xz --output wizer-v3.0.1-x86_64-linux.tar.xz
-tar -xJf wizer-v6.0.0-x86_64-linux.tar.xz
+curl --fail --location --silent https://github.com/bytecodealliance/wizer/releases/download/v6.0.0/wizer-v6.0.0-x86_64-linux.tar.xz --output wizer-v6.0.0.tar.xz
+tar -xvf "wizer-v6.0.0.tar.xz"
 export PATH=$(pwd)/wizer-v6.0.0-x86_64-linux:$PATH
