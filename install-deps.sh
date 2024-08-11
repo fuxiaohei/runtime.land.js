eb87032cf5196ec01b755c69d6f43399dd7aeb13
fuxiaohei <fuxiaohei@vip.qq.com>
2024-08-11 23:40:23 +0800

    use wasi-sdk-22.0 in actions

--- install-deps.sh
+++ install-deps.sh
@@ -3,9 +3,9 @@
 set -euo pipefail
 
 echo "Installing WASI-SDK"
-curl --fail --location --silent https://github.com/WebAssembly/wasi-sdk/releases/download/wasi-sdk-24/wasi-sdk-24.0-x86_64-linux.tar.gz --output wasi-sdk-24.0-linux.tar.gz
-tar -xzf wasi-sdk-24.0-linux.tar.gz
-#export WASI_SDK=$(pwd)/wasi-sdk-24.0
+curl --fail --location --silent https://github.com/WebAssembly/wasi-sdk/releases/download/wasi-sdk-22/wasi-sdk-22.0-x86_64-linux.tar.gz --output wasi-sdk-22.0-linux.tar.gz
+tar -xzf wasi-sdk-22.0-linux.tar.gz
+#export WASI_SDK=$(pwd)/wasi-sdk-22.0
 
 echo "Installing wizer"
 curl --fail --location --silent https://github.com/bytecodealliance/wizer/releases/download/v6.0.0/wizer-v6.0.0-x86_64-linux.tar.xz --output wizer-v6.0.0.tar.xz
